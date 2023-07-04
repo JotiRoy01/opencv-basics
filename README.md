187e438b217b110f4568a460e075d1fc4d10b76d
JotiRoy01 <125681739+JotiRoy01@users.noreply.github.com>
2023-07-04 12:20:15 +0600

    init setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# general_template
- general template for any project
+#Opencv-Basics
--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 echo [$(date)]: "START"
-export _VERSION_=3.7
+export _VERSION_=3.10
 echo [$(date)]: "creating environment with python ${_VERSION_}"
 conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
@@ -8,8 +8,8 @@ echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 # echo [$(date)]: "export conda environment"
 # conda env export > conda.yaml
-echo [$(date)]: "create an src/utils directory and files"
-mkdir -p src/utils && touch src/__init__.py src/main.py
-
+#echo [$(date)]: "create an src/utils directory and files"
+#mkdir -p src/utils && touch src/__init__.py src/main.py
+echo [$(date)]: "END"
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
